d151f07f5d92d356d24a654535e877d2da7f7b08
Nacho131104 <nacholopezamat@gmail.com>
2025-01-30 19:12:54 +0100

    update

--- resolvers.ts
+++ resolvers.ts
@@ -1,7 +1,15 @@
 
+import { Collection } from "mongodb";
+import {cityModel} from "./types.ts";
 
+type argsgetcities ={
+    context: Collection<cityModel>;
+}
 export const resolvers ={
     Query:{
-        default:():string => "Hola"
+        getCities:async(_:unknown,__:unknown,ctx:argsgetcities):Promise<cityModel[]|null> =>{
+            const cities = await ctx.context.find().toArray();
+            return cities;
+        }
     }
 }

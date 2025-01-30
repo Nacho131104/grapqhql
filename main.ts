2062de347d2c20b6fc7f675fd5cefae51c999a2a
Nacho131104 <nacholopezamat@gmail.com>
2025-01-30 18:59:20 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,10 +4,10 @@
 import {ApolloServer} from "@apollo/server"
 import {startStandaloneServer} from "@apollo/server/standalone";
 import { resolvers } from "./resolvers.ts";
-//import {Collection, MongoClient, ObjectId} from "mongodb"
+import { MongoClient} from "mongodb"
 import { schema } from "./schema.ts";
-//import {cityModel} from "./types.ts";
-/*
+import {cityModel} from "./types.ts";
+
 const mongoURL = Deno.env.get("mongoURL");
 
 if (!mongoURL) {
@@ -21,14 +21,14 @@ const dataBase = client.db('DataBase')
 
 const contact_Collection = dataBase.collection<cityModel>('Ciudades')
 
-*/
+
 const server = new ApolloServer({
   typeDefs : schema,
   resolvers
 });
 
 const { url } = await startStandaloneServer(server, {
-  context:async () => ({/*contact_Collection*/}),
+  context:async () => ({contact_Collection}),
   listen: { port: 9000 },
 });
 

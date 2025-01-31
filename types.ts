ad64c99b2ed83f39eb52d788add18f43dca73d31
Nacho131104 <nacholopezamat@gmail.com>
2025-01-31 10:35:46 +0100

    update

--- types.ts
+++ types.ts
@@ -12,3 +12,11 @@ export type cityModel=OptionalId<{
 }>
 
 
+//https://api.api-ninjas.com/v1/city
+export type APIcity ={
+    name: string,
+    latitude: number,
+    longitude: number,
+    population: number,
+    country: string,
+}

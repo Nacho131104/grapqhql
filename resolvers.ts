d52d19a9a649d26c8187ba4681b88990dfcd85c1
Nacho131104 <nacholopezamat@gmail.com>
2025-01-31 10:44:56 +0100

    update

--- resolvers.ts
+++ resolvers.ts
@@ -1,9 +1,15 @@
 
 import { Collection } from "mongodb";
 import {cityModel} from "./types.ts";
+import { ObjectId } from "../../../../AppData/Local/deno/npm/registry.npmjs.org/bson/6.10.1/bson.d.ts";
+import { GraphQLError } from "graphql";
 
 type argsgetcities ={
     contact_Collection: Collection<cityModel>;
+}
+
+type argsgetCity={
+    id: string,
 }
 export const resolvers ={
     Query:{
@@ -11,6 +17,24 @@ export const resolvers ={
             const cities = await ctx.contact_Collection.find().toArray();
             console.log(cities)
             return cities;
+        },
+
+        getCity:async(_:unknown,args:argsgetCity,ctx:argsgetcities):Promise<cityModel> =>{
+            const city = await ctx.contact_Collection.findOne({_id: new ObjectId(args.id)})
+            if(!city)throw new GraphQLError("No se ha encontrado dicha ciudad");
+            return city;
         }
+    },
+
+    /*
+    Ciudad:{
+        id:(parent:cityModel):string => {return parent._id!.toString();},
+
+        latitude:async(parent:cityModel):Promise<number>=>{
+
+
+        }
+
     }
+        */
 }

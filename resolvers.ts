86e609e3170d96c31fc0c1e72ecd11c1bef1b910
Nacho131104 <nacholopezamat@gmail.com>
2025-01-31 10:48:51 +0100

    update

--- resolvers.ts
+++ resolvers.ts
@@ -15,7 +15,6 @@ export const resolvers ={
     Query:{
         getCities:async(_:unknown,__:unknown,ctx:argsgetcities):Promise<cityModel[]|null> =>{
             const cities = await ctx.contact_Collection.find().toArray();
-            console.log(cities)
             return cities;
         },
 
